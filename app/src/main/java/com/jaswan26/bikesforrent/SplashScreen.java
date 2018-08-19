21eac84fad318991b5ce49bde4e67d0242a79b2e
Jaswan Hardaha <jaswan26@gmail.com>
2018-08-19 12:17:30 +0530

    Splash Screen Added

---
+++ app/src/main/java/com/jaswan26/bikesforrent/SplashScreen.java
@@ -0,0 +1,26 @@
+package com.jaswan26.bikesforrent;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+//        setTheme(R.style.splashScreenTheme);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(SplashScreen.this,MainActivity.class);
+               startActivity(mainIntent);
+               finish();
+            }
+        },2500);
+    }
+}
